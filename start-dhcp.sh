ea2d825c8ec6d7fe639c048da1ab506428b1f1fc
Patrick Kerwood <patrick@kerwood.dk>
2020-02-14 19:19:01 +0100

    Minor fix

--- start-dhcp.sh
+++ start-dhcp.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ -z ${CONFIG_PATH+x} ]; then
+    export CONFIG_PATH=/etc/dhcpd.conf
+fi
+
 touch /var/lib/dhcp/dhcpd.leases
-exec dhcpd -f -d $INTERFACES
+exec dhcpd -f -d $INTERFACES -cf $CONFIG_PATH
